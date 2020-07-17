c68a8b08332a3c1d2f8b4c8d593ed74822ebfcfc
Ivan Rybalko <ivribalko@gmail.com>
2020-07-17 11:49:43 -0400

    Openapi type

--- lib/src/pet_data.dart
+++ lib/src/pet_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:list_bloc/list_bloc.dart';
+import 'package:openapi/api.dart';
 import 'package:openapi/api/pet_api.dart';
 import 'package:openapi/model/pet.dart';
 
@@ -16,7 +17,7 @@ class PetBloc extends ListBloc<Pet, PetStatus> {
 class PetDataRepository implements ListRepository<Pet, PetStatus> {
   final PetApi _petApi;
 
-  PetDataRepository(openApi) : _petApi = openApi.getPetApi();
+  PetDataRepository(Openapi openApi) : _petApi = openApi.getPetApi();
 
   @override
   Future<List<Pet>> load({PetStatus filter}) => _petApi
@@ -27,7 +28,7 @@ class PetDataRepository implements ListRepository<Pet, PetStatus> {
 class PetPagedRepository implements PaginatedRepository<Pet, PetStatus> {
   final PetApi _petApi;
 
-  PetPagedRepository(openApi) : _petApi = openApi.getPetApi();
+  PetPagedRepository(Openapi openApi) : _petApi = openApi.getPetApi();
 
   @override
   Future<ListPage<Pet>> load({PetStatus filter}) => _petApi
